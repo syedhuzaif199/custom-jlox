05d2b61de0a77d39bc6ad2c2ceb048356e3c39a9
Syed Huzaif <syedhuzaif199@gmail.com>
2025-09-18 12:52:53 +0530

    Implement C-style ternary conditional operator
    - Add new token types QUESTION_MARK and COLON
    - Add new rule to the Scanner for '?' and ':' characters
    - Add new Expression type 'Ternary' to the GenerateAst
    - Implement AstPrinter and AstGraphViz Visitor patterns for the new Ternary operator
    - Implement new parsing function 'conditional' for the new ternary
      operator in the Parser
    - Check expression-grammar.txt for the new grammar and examples

---
+++ ternary-operator/src/lox/AstGraphViz.java
@@ -0,0 +1,79 @@
+package lox;
+
+import java.io.PrintWriter;
+
+import lox.Expr.Binary;
+import lox.Expr.Grouping;
+import lox.Expr.Literal;
+import lox.Expr.Ternary;
+import lox.Expr.Unary;
+
+class AstGraphViz implements Expr.Visitor<String>{
+    StringBuilder builder;
+    AstGraphViz() {
+        builder = new StringBuilder();
+    }
+    void outputDotFile(Expr expr, String filename) {
+        try {
+            PrintWriter writer = new PrintWriter(filename);
+            writer.write(print(expr));
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+            return;
+        }
+    }
+    String print(Expr expr) {
+        builder.append("digraph G {\n");
+        builder.append("node [shape=box];\n");
+        expr.accept(this);
+        builder.append("}\n");
+        return builder.toString();
+    }
+
+    public String visitBinaryExpr(Binary expr) {
+        String nodeId = "node" + System.identityHashCode(expr);
+        builder.append(String.format("%s [label=\"%s\"];\n", nodeId, expr.operator.lexeme));
+        String leftId = expr.left.accept(this);
+        String rightId = expr.right.accept(this);
+        builder.append(String.format("%s -> %s;\n", nodeId, leftId));
+        builder.append(String.format("%s -> %s;\n", nodeId, rightId));
+        return nodeId;
+    }
+
+    public String visitGroupingExpr(Grouping expr) {
+        String nodeId = "node" + System.identityHashCode(expr);
+        builder.append(String.format("%s [label=\"group\"];\n", nodeId));
+        String exprId = expr.expression.accept(this);
+        builder.append(String.format("%s -> %s;\n", nodeId, exprId));
+        return nodeId;
+    }
+
+    public String visitLiteralExpr(Literal expr) {
+        String nodeId = "node" + System.identityHashCode(expr);
+        String label = (expr.value == null) ? "nil" : expr.value.toString();
+        builder.append(String.format("%s [label=\"%s\"];\n", nodeId, label));
+        return nodeId;
+    }
+
+    public String visitUnaryExpr(Unary expr) {
+        String nodeId = "node" + System.identityHashCode(expr);
+        builder.append(String.format("%s [label=\"%s\"];\n", nodeId, expr.operator.lexeme));
+        String rightId = expr.right.accept(this);
+        builder.append(String.format("%s -> %s;\n", nodeId, rightId));
+        return nodeId;
+    }
+    @Override
+    public String visitTernaryExpr(Ternary expr) {
+        String nodeId = "node" + System.identityHashCode(expr);
+        builder.append(String.format("%s [label=\"%s\"];\n", nodeId, expr.operator1.lexeme + expr.operator2.lexeme));
+        String firstId = expr.first.accept(this);
+        String secondId = expr.second.accept(this);
+        String thirdId = expr.third.accept(this);
+        builder.append(String.format("%s -> %s;\n", nodeId, firstId));
+        builder.append(String.format("%s -> %s;\n", nodeId, secondId));
+        builder.append(String.format("%s -> %s;\n", nodeId, thirdId));
+        return nodeId;
+
+    }
+}
